53512db13f0442145715d6448c4069a9ba20f5f7
Lawton Skaling <lskaling@icloud.com>
2022-12-19 21:55:56 -0900

    praising and tracking points correctly

--- PraiseBot.py
+++ PraiseBot.py
@@ -45,7 +45,24 @@ def replaceMention(text):
         return text
     else:
         return text
+
+def convertPromptToText(text):
+    text = text.replace("><@", "> and <@")
+    text = text.replace("> <@", "> and <@")
+    while("<@" in text):
+        text = replaceMention(text)
+    return text
+
+def getUsersFromText(text):
+    users = []
+    while("<@" in text):
+        mention = text[text.find("<@"):text.find(">")+1]
+        users.append(mention)
+        text = text.replace(mention, "")
+    return users
     
+def removePraiseBotText(text):
+    return text.replace("Praise Bot", "")
 
 @slack_event_adapter.on('app_mention')
 def mention(payload):
@@ -54,13 +71,14 @@ def mention(payload):
     event = payload.get('event', {})
     channel_id = event.get('channel')
     user_id = event.get('user')
-
     text = event.get('text')
-    text = text.replace("<@U04FP0Z01QV>", "")
 
-    name = getNameFromMention(text)
-    #remove bot name from text
-    text = replaceMention(text)
+    prompt = convertPromptToText(text)
+    prompt = removePraiseBotText(prompt)
+    users = getUsersFromText(text)
+    
+    print(prompt)
+    print(users)
 
     cnx = mysql.connector.connect(
         host="iu51mf0q32fkhfpl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
@@ -69,42 +87,51 @@ def mention(payload):
         database="azdlxzpzmqhqjfyh"
     )
 
+
     cursor = cnx.cursor()
+
+    print("users: " + str(users[0]))
+
+    user = users[0].replace("<@", "").replace(">", "")
 
     query = "SELECT points FROM users WHERE id = 'U02GG2K5XLH';"
     values = ()
     cursor.execute(query, values)
-    result = cursor.fetchone()
+    points = cursor.fetchone()[0] + 1
+
+    updateQuery = "UPDATE users SET points = " + str(points) + " WHERE id = 'U02GG2K5XLH';"
+    cursor.execute(updateQuery)
+    cnx.commit()
 
     cursor.close()
     cnx.close()
 
-    response = generateText(text, name)
+    response = generateText(prompt)
 
     try:
     # Use the WebClient to send a message to the channel
         response = client.chat_postMessage(
             channel=channel_id,
-            text="test"#response + "\n\nNice! " + name + ", now at " + str(result[0]) + " points",#generateText(text, name),  # Include the command text in the response
-            # blocks = [
-            #     {
-            #         "type": "section",
-            #         "text": {
-            #             "type": "mrkdwn",
-            #             "text": response
-            #         }
-            #     },
-            #     {
-            #         "type": "divider"
-            #     },
-            #     {
-            #         "type": "section",
-            #         "text": {
-            #             "type": "mrkdwn",
-            #             "text": name + ", now at " + str(result[0]) + " points"
-            #         }
-            #     }
-            # ]
+            text="praise",#response + "\n\nNice! " + name + ", now at " + str(result[0]) + " points",#generateText(text, name),  # Include the command text in the response
+            blocks = [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": response
+                    }
+                },
+                {
+                    "type": "divider"
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": users[0] + ", now at " + str(points) + " points"
+                    }
+                }
+            ]
             
         )
     except SlackApiError as e:
@@ -114,14 +141,16 @@ def mention(payload):
     return Response(), 200
 
 
-def generateText(message, name):
+def generateText(message):
         
     ## options
     response_options = ["a short humorous thank you", "a short serious thank you", "a poem", "a haiku", "a rap", "a space themed thank you", "a thank you with a space pun"]
 
     prompt_choice = random.choice(response_options)
 
-    prompt = "write" + prompt_choice + " to " + name + " for " + message
+    prompt = "write " + prompt_choice + " to " + message
+
+    print("prompt: " + prompt)
 
     completion = openai.Completion.create(
         engine="text-davinci-002",
