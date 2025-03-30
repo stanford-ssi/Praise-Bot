13dbc800937381ae99f5defb6d05a2ef67e0aef4
Lawton Skaling <lskaling@icloud.com>
2025-03-29 17:20:28 -0700

    Fixed health check

--- PraiseBotServer/health_check.py
+++ PraiseBotServer/health_check.py
@@ -1,14 +1,6 @@
-from flask import request, jsonify
-
 def setup_health_check(flask_app, handler):
 
     @flask_app.route('/health', methods=['GET'])
     def health_check():
         print("Health check received.")
-
-        if False:#TODO Implement database check
-            return jsonify({
-                "message": "No response from database."
-            }), 503  # Return HTTP 200 if all nodes are online
-        # If all nodes are online
-        return 200
+        return "OK", 200
--- PraiseBotV2.py
+++ PraiseBotV2.py
@@ -17,19 +17,19 @@ setup_slack_commands(apps.flask_app, apps.slack_app, apps.handler)
 setup_health_check(apps.flask_app, apps.handler)
 
 if __name__ == "__main__":
-    test_body = {
-        "text": "<@U02GG2K5XLH|lskaling> for cleaning the workspace",
-        "channel_id": "C7F3VVB1S"
-    }
+    # test_body = {
+    #     "text": "<@U02GG2K5XLH|lskaling> for cleaning the workspace",
+    #     "channel_id": "C7F3VVB1S"
+    # }
 
-    test_client = WebClient(token=os.environ["SLACK_API_TOKEN"])
+    # test_client = WebClient(token=os.environ["SLACK_API_TOKEN"])
 
-    # call the handler directly
-    praise(
-        ack=lambda: print("ACK"),
-        body=test_body,
-        respond=print,
-        client=test_client
-    )
+    # # call the handler directly
+    # praise(
+    #     ack=lambda: print("ACK"),
+    #     body=test_body,
+    #     respond=print,
+    #     client=test_client
+    # )
 
     apps.flask_app.run(port=3000)
